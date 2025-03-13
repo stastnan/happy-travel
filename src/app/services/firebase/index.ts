ce6606bc66c22887f3429c2aa9593dd216329a29
Nikola Stastna <82889664+stastnan@users.noreply.github.com>
2025-03-13 15:07:06 +0100

    feat: save wizard data to firestore (#19)

--- src/app/services/firebase/index.ts
+++ src/app/services/firebase/index.ts
@@ -1,4 +1,4 @@
-export { auth } from "./firebase";
+export { auth, firestore } from "./firebase";
 export { mapAuthCodeToMessage } from "./helpers/mapAuthCodeToMessage";
 export { useAuthStateSubscription } from "./hooks/useAuthStateSubscription";
 export { useStorage } from "./hooks/useStorage";
